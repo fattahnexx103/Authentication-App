5498fdd2b39f5c990f165e466f9612b5acba9880
fattahnexx103 <neehadfattah@yahoo.com>
2020-03-31 22:21:25 -0400

    Created Nav Graph and Initial Login Fragment Layouts

---
+++ app/src/main/java/com/nexx/nexxassistant/nexxauthenticator/view/SignUpFragment.kt
@@ -0,0 +1,26 @@
+package com.nexx.nexxassistant.nexxauthenticator.view
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import com.nexx.nexxassistant.nexxauthenticator.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class SignUpFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+
+
+}
